a69497c5b9e350a234e5df5c14de29118b568330
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2024-03-21 00:32:59 +0000

    1

---
+++ html-js-css-example-1/ourJavaScriptFile.js
@@ -0,0 +1,8 @@
+console.log("The site is connected to JS and running it.");
+
+function heyGuys() {
+	alert("What's up Essence and Kandrea!");
+	console.log("The button is connected and running.");
+}
+
+// document.getElementById("myDiv")

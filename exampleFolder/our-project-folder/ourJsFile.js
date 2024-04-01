6399cb2bdba412744af47b3eccc9634e11174bf6
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2024-04-01 23:13:20 +0100

    3

---
+++ exampleFolder/our-project-folder/ourJsFile.js
@@ -0,0 +1,12 @@
+//let exampleVariable = [14, 16, 18, ["Bob", "Mary"]];
+
+let myFriendJake = {
+	firstName: "Jake",
+	lastName: "Miller",
+	calledLastTime: "10/10/1992",
+	hospitalVisits: ["Jan 2012", "Feb 2022"],
+};
+
+function ourGreatFunction() {
+	console.log(exampleVariable);
+}
